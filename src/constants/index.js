6a2608a3676ac374867015d928b1db10a8977607
Patihan Prasongphol <patihan70@gmail.com>
2024-01-26 16:07:39 +0700

    modifyConstants

--- src/constants/index.js
+++ src/constants/index.js
@@ -185,7 +185,7 @@ import {
       iconBg: "#E6DEDD",
       date: "Aug 2023 - Present",
       points: [
-        "POPSLOTH application via Nextjs, Tailwind CSS, Typescript, Vercel deployment.",
+        "POPSLOTH application via Nextjs, Tailwind CSS, Typescript, Vercel deployment. *Got 'Origin Selected' reward.*",
         "Portfolio Website via React vite, Tailwind CSS, FireBase, React Three Fiber, Three.js.",
         "House keeper robot via microbit, huskylens, servo, 3D print and laser cut.",
         "Inventory Management System for Owner and cashier of a shop via c.",
